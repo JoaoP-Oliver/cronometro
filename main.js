4ef3e3ee79fe5e49541c24d6ad3d59b815f15830
João Pedro Oliveira <88394758+JoaoP-Oliver@users.noreply.github.com>
2022-01-09 16:07:47 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+let time;
+let counter = document.querySelector('h1')
+
+let ss = 0
+let mm = 0
+let hh = 0
+
+//Iniciar o cronometro
+function start() {
+  time = setInterval(() => {
+    timer()
+  },1000)
+}
+
+//Pausar o cronometro
+function pause() {
+  clearInterval(time)
+}
+
+//Zerar o cronometro
+function clear() {
+  ss = 0
+  mm = 0
+  hh = 0
+  clearInterval(time)
+}
+
+function timer() {
+  ss++
+
+  if(ss == 60) {
+    ss = 0
+    mm++
+
+    if(mm == 60) {
+      mm = 0
+      hh++
+    }
+  }
+
+  let format = `${hh < 10 ? '0' + hh : hh}:${mm < 10 ? '0' + mm : mm}:${ss < 10 ? '0' + ss : ss}`
+  counter.innerText = format
+}
