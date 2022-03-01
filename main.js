4fc7664d4a266901c4b340072d584431cf89f82b
João Pedro Oliveira <88394758+JoaoP-Oliver@users.noreply.github.com>
2022-02-28 23:19:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,19 @@
-let time;
-let counter = document.querySelector('h1')
+let ss = 0
+        let mil = 0
 
-let ss = 0 //segundos
-let mm = 0//minutos
-let hh = 0//horas
+        const doom = setInterval(() => {
+            time()
+        },10)
 
-//Iniciar o cronometro
-function start() {
-  time = setInterval(() => {
-    timer()
-  },1000)
-}
+        function time() {
+            mil++
+            
+            if(mil === 99) {
+                ss++
+                mil = 0
+            }
 
-//Pausar o cronometro
-function pause() {
-  clearInterval(time)
-}
-
-//Zerar o cronometro
-function clear() {
-  ss = 0
-  mm = 0
-  hh = 0
-  clearInterval(time)
-}
-
-function timer() {
-  ss++
-
-  if(ss == 60) {
-    ss = 0
-    mm++
-
-    if(mm == 60) {
-      mm = 0
-      hh++
-    }
-  }
-
-  let format = `${hh < 10 ? '0' + hh : hh}:${mm < 10 ? '0' + mm : mm}:${ss < 10 ? '0' + ss : ss}`
-  counter.innerText = format
-}
+            console.log(ss)
+            let time = document.getElementById('time')
+            time.innerHTML = `${ss}:${mil}`
+        }
