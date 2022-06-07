3421289bfc9af94e1f895e8a60baf382997462ed
João Pedro Oliveira <88394758+JoaoP-Oliver@users.noreply.github.com>
2022-06-07 15:56:12 -0300

    Update main.js
    Adicionada bloco de codigo para mais tarde fazer uma implementação do mesmo

--- main.js
+++ main.js
@@ -17,3 +17,19 @@ let ss = 0
             let time = document.getElementById('time')
             time.innerHTML = `${ss}:${mil}`
         }
+
+
+// Acrescentar mais tarde
+/*
+                let time = 22
+
+                if(time > 0 && time < 6) {
+                  console.log('dark purple')
+                } else if(time >= 6 && time <= 12) {
+                  console.log('yellow')
+                } else if(time >= 12 && time <= 18) {
+                  console.log('gray')
+                } else if(time >= 18 && time <= 24) {
+                  console.log('black')
+                }
+*/
